81ddee9f5e2997437f949ff22f7f708c0503cf78
root <root@MERIGNAC-DESKTOP.localdomain>
2019-01-05 11:04:17 +0100

    ref fix header

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="google-site-verification" content="08LdDZPUhZkIv0Vz8eRG4h-lEazendao5JDHJlZBdrA" />
-		<meta name="description" content="Aménagement, transformation de véhicules utilitaires" />
+		<meta name="description" content="Entreprise de nettoyage pour particuliers et professionnels (vitres, locaux, chantiers)" />
 		<link rel="icon" href="img/favicon.ico" type="image/x-icon" />
 		<link rel="stylesheet" href="/css/foundation.css" />
 		<link rel="stylesheet" href="/css/swiper.css">
